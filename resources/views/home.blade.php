8f439e846a76e6e1315fd45336e6042dba8eaeca
Brian Mounmanivong <brian@tradesy.com>
2018-12-28 00:26:13 -0800

    testing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,14 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <link rel="stylesheet" href="css/app.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home</title>
 </head>
 <body>
 <div class="flex-center position-ref full-height">
-    <div class="content">
+    <div class="content red">
         Hello
     </div>
 </div>
